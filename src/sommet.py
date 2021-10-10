0ffc8f55c79a7588af18af703d0c454953f58184
Jihad Ghannoum <69480541+Ghannoum99@users.noreply.github.com>
2021-10-10 12:06:39 +0200

    Delete sommet.py

--- src/sommet.py
+++
@@ -1,9 +0,0 @@
-class Sommet:
-	def __init__(self, n):
-		self.nom = n
-		self.voisins = list()
-
-	def ajouter_voisin(self, v):
-		if v not in self.voisins:
-			self.voisins .append((v))
-			self.voisins .sort()
