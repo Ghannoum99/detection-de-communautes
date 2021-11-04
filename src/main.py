7c43db822d67286c2d6c6f8688fca4660f253a12
Ghannoum99 <ghannoumjihad59@gmail.com>
2021-11-04 21:04:16 +0100

    modification

--- src/main.py
+++ src/main.py
@@ -13,16 +13,16 @@ if __name__ == '__main__':
     print("\n*************************** SANS PIVOT ***************************")
     print(list(graphe.bron_kerbosch_sans_pivot(list(graphe.liste_adjacence.keys()), R, X)))
 
-    print("\n*************************** AVEC PIVOT ***************************")
+    print("\n%%%%%%%%%%%%%%%%%%%%%%%  AVEC PIVOT %%%%%%%%%%%%%%%%%%%%%%%")
     print(list(graphe.bron_kerbosch_avec_pivot(list(graphe.liste_adjacence.keys()), R, X)))
 
-    print("\n********************* AVEC ORDRE DE DEGENERESCENCE ********************")
+    print("\n%%%%%%%%%%%%%%%%%%%%%%% AVEC ORDRE DE DEGENERESCENCE %%%%%%%%%%%%%%%%%%%%%%%")
     print(list(graphe.version_avec_ordonnancement()))
 
-    print("\n********************* ENUMERATION CLIQUES MAXIMALES ********************")
+    print("\n%%%%%%%%%%%%%%%%%%%%%%% ENUMERATION CLIQUES MAXIMALES %%%%%%%%%%%%%%%%%%%%%%%")
     print(graphe.enumeration_cliques_max())
     
-    print("\n****************** ENUMERATION CLIQUES MAXIMALES V2 *****************")
+    print("\n%%%%%%%%%%%%%%%%%%%%%%%  ENUMERATION CLIQUES MAXIMALES V2 %%%%%%%%%%%%%%%%%%%%%%%")
     print(list(graphe.enumeration_cliques_max_2()))
     
     # Dessiner des graphes
