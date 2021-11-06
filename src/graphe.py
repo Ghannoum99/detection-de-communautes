05abb20584f8861b17bceb0eff8a141c152aa39c
Ghannoum99 <ghannoumjihad59@gmail.com>
2021-11-06 23:44:44 +0100

    Corriger fct pivot_tomita

--- src/graphe.py
+++ src/graphe.py
@@ -229,11 +229,11 @@ class Graphe:
         # initialiser le degree max à la taille de la liste ( P inter N(u) )
         degree_max = len(P_inter_voisin_de_u)
 
-        # P Union X privée de N(u)
-        # P union X \ N(u)
-        P_privee_de_voisins_de_u = list(set(P + X) - set(self.get_voisin(u)))
+        # P Union X privée de u
+        # P union X \ {u}
+        P_privee_de_u = list(set(P + X) - {u})
 
-        for v in P_privee_de_voisins_de_u:
+        for v in P_privee_de_u:
             # L'intersection de P et N(v)
             P_inter_voisin_de_v = list(set(P) & set(self.get_voisin(v)))
             if len(P_inter_voisin_de_v) > degree_max:
